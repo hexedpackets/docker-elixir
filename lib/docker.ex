af80c7130265df9908cbc17e704f1a81db92c7d0
William Huba <william@trywildcard.com>
2015-05-13 19:38:05 -0400

    Move misc commands to a new module

--- lib/docker.ex
+++ lib/docker.ex
@@ -1,21 +1,2 @@
 defmodule Docker do
-  @doc """
-  Display system-wide information.
-  """
-  def info, do: Docker.Client.get("/info")
-
-  @doc """
-  Show the docker version information.
-  """
-  def version, do: Docker.Client.get("/version")
-
-  @doc """
-  Ping the docker server.
-  """
-  def ping, do: Docker.Client.get("/_ping")
-
-  @doc """
-  Monitor Docker's events.
-  """
-  def events(since), do: Docker.Client.get("/events?since=#{since}")
 end
