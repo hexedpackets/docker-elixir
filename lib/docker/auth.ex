aab7c4ba262fb02af1f7b25b734dea529f8347b8
mrdotb <baptiste.chaleil@gmail.com>
2017-06-28 14:14:48 +0200

    Fix docker.auth

--- lib/docker/auth.ex
+++ lib/docker/auth.ex
@@ -1,17 +1,8 @@
 defmodule Docker.Auth do
-  @default_server "https://index.docker.io/v1/"
-
   @doc """
-  Authenticate to the Docker registry.
+  Authenticate to a docker registry.
   """
-  def login(credentials = %{"email" => _email, "password" => _password, "username" => _username, "serveraddress" => _server}) do
-    data = Poison.encode!(credentials)
-    Docker.Client.post("/auth", data)
-  end
-
   def login(credentials) do
-    credentials
-    |> Dict.put("serveraddress", @default_server)
-    |> login
+    Docker.Client.post("/auth", credentials)
   end
 end
