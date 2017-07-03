c1083f035109e81b29dd0ce507e7e56fc35fa2b2
mrdotb <baptiste.chaleil@gmail.com>
2017-07-03 11:29:36 +0200

    Pull a random image.

--- test/images_test.exs
+++ test/images_test.exs
@@ -1,7 +1,7 @@
 defmodule ImagesTest do
   use ExUnit.Case
 
-  @test_image "busybox"
+  @test_image "bitnami/redis"
   @test_image_tag "latest"
 
   setup_all do
@@ -13,11 +13,19 @@ defmodule ImagesTest do
   test "list images" do
     {:ok, images} = Docker.Images.list()
     assert is_list(images)
-    #filtered = images |> Enum.filter(fn(image) -> "#{@test_image}:#{@test_image_tag}" in image["RepoTags"] end)
-    #assert length(filtered) > 0
   end
 
   test "inspect image" do
     assert {:ok , _} = Docker.Images.inspect(@test_image)
+  end
+
+  test "streaming pulling" do
+    Docker.Images.delete(@test_image)
+    assert %HTTPoison.AsyncResponse{id: id} =
+      Docker.Images.pull_stream(@test_image)
+    assert_receive %HTTPoison.AsyncStatus{id: id, code: 200}, :infinity
+    assert_receive %HTTPoison.AsyncHeaders{id: id, headers: _}, :infinity
+    assert_receive %HTTPoison.AsyncChunk{id: id, chunk: _}, :infinity
+    assert_receive %HTTPoison.AsyncEnd{id: id}, :infinity
   end
 end
