e3522f8697ad307ad6b67d4dcfd666557b3e7c43
mrdotb <baptiste.chaleil@gmail.com>
2017-07-03 11:31:17 +0200

    mist test stream event.

---
+++ test/misc_test.exs
@@ -0,0 +1,25 @@
+defmodule MiscTest do
+  use ExUnit.Case
+
+  @test_image "busybox"
+  @test_image_tag "latest"
+
+  setup_all do
+    IO.puts "Pulling #{@test_image}:#{@test_image_tag} for testing..."
+    Docker.Images.pull(@test_image, @test_image_tag)
+    :ok
+  end
+
+  test "events stream" do
+    %HTTPoison.AsyncResponse{id: id} = Docker.Misc.events_stream()
+    conf = %{"AttachStdin" => false,
+      "Env" => [],
+      "Image" => "#{@test_image}:#{@test_image_tag}",
+      "Volumes" => %{},
+      "ExposedPorts" => %{},
+    }
+    assert {:ok, container} = Docker.Containers.create(conf, "misc")
+    assert_receive %HTTPoison.AsyncChunk{id: id, chunk: _}, :infinity
+    Docker.Containers.remove(container["Id"])
+  end
+end
