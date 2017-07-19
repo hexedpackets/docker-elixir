7fa70060ba2b4c792aefd7da08a4efeb3b6d84e2
Laurent <lau.cazanove@gmail.com>
2017-07-19 15:00:34 +0200

    Refactor and test misc info, version and ping

--- lib/docker/misc.ex
+++ lib/docker/misc.ex
@@ -2,22 +2,49 @@ defmodule Docker.Misc do
   @doc """
   Display system-wide information.
   """
-  def info, do: Docker.Client.get("/info")
+  def info do
+    Docker.Client.get("/info") |> decode_system_response
+  end
 
   @doc """
   Show the docker version information.
   """
-  def version, do: Docker.Client.get("/version")
+  def version do
+    Docker.Client.get("/version") |> decode_system_response
+  end
+
+  defp decode_system_response(%HTTPoison.Response{body: body, status_code: status_code}) do
+    # Logger.debug "Decoding Docker API response: #{inspect body}"
+    case Poison.decode(body) do
+      {:ok, dict} ->
+        case status_code do
+          200 -> {:ok, dict}
+          500 -> {:error, "Server error"}
+        end
+      {:error, message} ->
+        {:error, message}
+    end
+  end
 
   @doc """
   Ping the docker server.
   """
-  def ping, do: Docker.Client.get("/_ping")
+  def ping do
+    Docker.Client.get("/_ping") |> decode_ping_response
+  end
+
+  defp decode_ping_response(%HTTPoison.Response{body: body, status_code: status_code}) do
+    case status_code do
+      200 -> {:ok, body}
+      500 -> {:error, "Server error"}
+    end
+  end
 
   @doc """
-  Monitor Docker's events.
+  Monitor Docker's events since given timestamp.
   """
   def events(since), do: Docker.Client.get("/events?since=#{since}")
+  
   @doc """
   Monitor Docker's events as stream.
   """
