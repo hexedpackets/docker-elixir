bfc45ba79315066cfbf92c6a72110b7e86b9ed6e
mrdotb <baptiste.chaleil@gmail.com>
2017-07-21 13:21:30 +0200

    replace @default_headers with a function because overload.

--- lib/docker/client.ex
+++ lib/docker/client.ex
@@ -7,12 +7,12 @@ defmodule Docker.Client do
     "#{host}/#{version}"
   end
 
-  @default_headers %{"Content-Type" => "application/json"}
+  defp default_headers, do: %{"Content-Type" => "application/json"}
 
   @doc """
   Send a GET request to the Docker API at the speicifed resource.
   """
-  def get(resource, headers \\ @default_headers) do
+  def get(resource, headers \\ default_headers()) do
     full = base_url() <> resource
     Logger.debug "Sending GET request to the Docker HTTP API: #{full}"
     full
@@ -22,7 +22,7 @@ defmodule Docker.Client do
   @doc """
   Send a POST request to the Docker API, JSONifying the passed in data.
   """
-  def post(resource, data \\ "", headers \\ @default_headers) do
+  def post(resource, data \\ "", headers \\ default_headers()) do
     Logger.debug "Sending POST request to the Docker HTTP API: #{resource}, #{inspect data}"
     data = Poison.encode! data
     Logger.debug("Posting #{inspect(base_url() <> resource)}")
@@ -33,7 +33,7 @@ defmodule Docker.Client do
   @doc """
   Send a request with the verb to the Docker API, stream the response.
   """
-  def stream(verb, resource, data \\ "", headers \\ @default_headers) do
+  def stream(verb, resource, data \\ "", headers \\ default_headers()) do
     Logger.debug "Sending #{verb} request to the Docker HTTP API: #{resource}, #{inspect data}"
     data = Poison.encode! data
     url = base_url() <> resource
@@ -44,7 +44,7 @@ defmodule Docker.Client do
   @doc """
   Send a DELETE request to the Docker API.
   """
-  def delete(resource, headers \\ @default_headers) do
+  def delete(resource, headers \\ default_headers()) do
     Logger.debug "Sending DELETE request to the Docker HTTP API: #{resource}"
     base_url() <> resource
     |> HTTPoison.delete!(headers)
